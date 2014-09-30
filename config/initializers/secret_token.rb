10ef972d4c269c70af46c5a64f24e3d0f85fe4dd
Alexander Vasyuk <alexandervasyuk@gmail.com>
2014-09-29 23:13:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iconlab::Application.config.secret_token = '279102b69bcece17ea328f5a64baa4c129eb3fe7a4e077cabedd0144c45b29a202ddd3594fba2b14a8123e927fd04e82cf4e25c521e6420aedecfe37c318eb4e'
